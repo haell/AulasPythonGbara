df751623d3169f6ffcdafcac854969fd97f7b7dc
Haell <israelaraujo.emply@gmail.com>
2020-09-23 19:31:43 -0300

    APRENDENDO Ler o nome completo Imprimir o nome com todas as letras maiúsculas Imprimir o nome com todas as letras minúsculas Quantas letras ao todo (sem considerar os espaços) Quantas letras tem o primeiro nome

---
+++ ex022.py
@@ -0,0 +1,17 @@
+# Ler o nome completo
+# Imprimir o nome com todas as letras maiúsculas
+# Imprimir o nome com todas as letras minúsculas
+# Quantas letras ao todo (sem considerar os espaços)
+# Quantas letras tem o primeiro nome
+
+# nome = str(input('Digite seu nome completo: '))
+# print('Seu nome com todas as letras em maiúsculo = ', nome.upper())
+# print('Seu nome com todas as letras em minúsculo = ', nome.lower())
+# print('Seu nome tem {} letras (contando os espaços em branco)'.format(len(nome)))
+# print('Seu nome tem {} letras (Sem os espaços em branco)'.format(len(''.join(nome.split()))))
+# print('"Extra": Seu nome tem {} palavras'.format(len(nome.split())))
+
+# Resolvendo o exercicio todo em duas linhas.
+nome = str(input('Digite seu nome completo: '))
+nome1 = nome.split()
+print("""Seu nome com todas as letras em maiúsculo = {}\nSeu nome com todas as letras em minúsculo = {}\nSeu nome tem {} letras (contando os espaços em branco)\nSeu nome tem {} letras (Sem os espaços em branco)\n"Extra": Seu nome tem {} palavras\nSeu primeiro nome tem {} letras""".format(nome.upper(), nome.lower(), len(nome), len(''.join(nome.split())), len(nome.split()), len(nome1[0])))
