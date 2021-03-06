4022eebea44b69e489d95a58ffe3d09f2ed0fa17
HæLL <israelaraujo.emply@gmail.com>
2021-03-05 21:43:53 -0300

    Testando código de calculo "seno, cosseno e tangente" de outra forma.

--- testedainternet.py
+++ testedainternet.py
@@ -12,81 +12,45 @@ import time
 import math
 
 print("\t SENO, COSSENO E TANGENTE: \n")
-
 a = input("SENO ----> \n'1'\n"
           "COSSENO ----> \n'2'\n"
           "TANGENTE ----> \n'3'\n"
           "SAIR ----> \n'n'\n")
-
 if a == 'n':
     print("\t CARREGANDO....\n")
-
     loop = 0
     time.sleep(1.2)
-
     exit()
-
 
 elif a == '1':
     print("\t CARREGANDO....\n")
-
-    loop = 0
     time.sleep(1.2)
-
-    print("\t SENO: \n")
-
+    print("\t SENO \n")
     b = input("DIGITE O VALOR DO ÂNGULO: ")
-
-    resp1 = float(b)
-    resp = math.sin(resp1)
-
+    resp = math.sin(float(b))
     print("\t CALCULANDO....\n")
-
     loop = 0
     time.sleep(1.2)
-
-    print("\t O VALOR DO SENO DO ÂNGULO", b, "É:", resp, "\n")
-
-
+    print("\t O VALOR DO SENO DO ÂNGULO {}° É: {:.2f} \n".format(b, resp))
 
 elif a == '2':
     print("\t CARREGANDO....\n")
-
     loop = 0
     time.sleep(1.2)
-
-    print("\t COSSENO: \n")
-
+    print("\t COSSENO \n")
     b = input("DIGITE O VALOR DO ÂNGULO: ")
-
-    resp1 = float(b)
-    resp = math.cos(b)
-
+    resp = math.cos(float(b))
     print("\t CALCULANDO....\n")
-
-    loop = 0
     time.sleep(1.2)
-
-    print("\t O VALOR DO COSSENO DO ÂNGULO", b, "É:", resp, "\n")
-
-
+    print("\t O VALOR DO COSSENO DO ÂNGULO {}° É: {:.2f} \n".format(b, resp))
 
 elif a == '3':
     print("\t CARREGANDO....\n")
-
     loop = 0
     time.sleep(1.2)
-
-    print("\t TANGENTE: \n")
-
+    print("\t TANGENTE \n")
     b = input("DIGITE O VALOR DO ÂNGULO: ")
-
-    resp1 = float(b)
-    resp = math.tan(b)
-
+    resp = math.tan(float(b))
     print("\t CALCULANDO....\n")
-
-    loop = 0
     time.sleep(1.2)
-
-    print("\t O VALOR DA TANGENTE DO ÂNGULO", b, "É:", resp, "\n")
+    print("\t O VALOR DA TANGENTE DO ÂNGULO {}° É: {:.2f} \n".format(b, resp))
