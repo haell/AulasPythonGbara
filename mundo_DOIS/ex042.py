65e3abc189d71190037161438fbf68baef866513
HæLL <israelaraujo.emply@gmail.com>
2023-04-25 15:09:27 -0300

    Alteração do print

--- mundo_DOIS/ex042.py
+++ mundo_DOIS/ex042.py
@@ -44,8 +44,8 @@ def testa_tipo_triangulo(segmento_1, segmento_2, segmento_3):
 
 
 if reta_1 < reta_2 + reta_3 and reta_2 < reta_1 + reta_3 and reta_3 < reta_1 + reta_2:
-    print(f'As retas {reta_1:.0f}, {reta_2:.0f} e {reta_3:.0f}: \
-\033[1;34;40mPODEM\033[m formar um triângulo do tipo \033[4;33;40m{testa_tipo_triangulo(reta_1, reta_2, reta_3)}\033[m\n')
+    print(f'As retas {reta_1:.0f}, {reta_2:.0f} e {reta_3:.0f}: \033[1;34;40mPODEM\033[m formar um triângulo do tipo', end='')
+    print(f' \033[4;33;40m{testa_tipo_triangulo(reta_1, reta_2, reta_3)}\033[m\n')
     
 else:
     print(f'As retas {reta_1:.0f}, {reta_2:.0f} e {reta_3:.0f}: \033[1;31;40mNÃO\033[m formam um triângulo!\n')
