19a4b6e2a3875220dc61a4ea021bdf9e0bafd0a7
HæLL <israelaraujo.emply@gmail.com>
2023-05-31 19:06:34 -0300

    Aula de Dicionários

---
+++ mundo_TRES/Aula19.py
@@ -0,0 +1,20 @@
+""" dados = dict
+dados = {
+    'nome':'Haell',
+    'idade': 41
+}
+print(dados)
+print(f"Nome: {dados['nome']}\nIdade: {dados['idade']}\n")
+print('-'*36)
+dados['sexo'] = 'M'
+print(f"Nome: {dados['nome']}\nIdade: {dados['idade']}\n")print('-'*36)
+print(dados)
+print('-'*36)
+ """
+
+pessoas = {
+    'nome':'Haell',
+    'idade': 41,
+    'sexo':'M'
+    }
+print()
