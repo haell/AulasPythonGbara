40f07ad9308b2c49a7572f440afda199a5ca1757
HæLL <israelaraujo.emply@gmail.com>
2023-06-13 13:59:22 -0300

    Funções parte 2

---
+++ mundo_TRES/Aula21.py
@@ -0,0 +1,8 @@
+def soma(a=0, b=0, c=0):
+    soma = a + b + c
+    return soma
+
+resp1 = soma(5, 3, 2)
+resp2 = soma(3, 2)
+resp3 = soma(2)
+print(f"Meus calcúlos deram: {resp1}, {resp2} e {resp3}")
