52525d6d1765773964ec075968c80522856a1d38
HæLL <israelaraujo.emply@gmail.com>
2023-06-16 15:52:21 -0300

    REFACTORE: Otimizando as funções

--- mundo_TRES/fex107/util.py
+++ mundo_TRES/fex107/util.py
@@ -1,2 +1,10 @@
 def ln(x='-', y=100):
     print(x*int(y))
+    print()
+
+
+def rotulo(frase='RÓTULO'):
+    print(f"-"*(len(frase)+15))
+    texto_rotulo = '{:^30}'.format(f'{frase}')
+    print(texto_rotulo.upper())
+    print(f"-"*(len(frase)+15))
