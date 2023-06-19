b3270765c972bc2c3e73ee55f2b04b63705b529e
HæLL <israelaraujo.emply@gmail.com>
2023-06-19 15:05:08 -0300

    REFACTORE: Implementando mais código no execício 115

--- mundo_TRES/ex115/sistema.py
+++ mundo_TRES/ex115/sistema.py
@@ -3,20 +3,31 @@ import os
 from time import sleep
 from rich import print
 from lib.interface import *
+from lib.arquivo import *
+
+nome_arquivo = 'cursoemvideo.txt'
+if not arquivo_existe(nome_arquivo):
+    criar_arquivo(nome_arquivo)
+     
 
 while True:
         resposta = montar_menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair'])
         if resposta == 1:
             os.system('cls')
-            rotulo("Opção 1", 2)
+            # Opção de listar as pessoas cadastradas
+            ler_arquivo(nome_arquivo)
         elif resposta == 2:
             os.system('cls')
-            rotulo("Opção 2", 2)
+            # Opção de cadastrar pessoa no arquivo.
+            rotulo('CADASTRAR PESSOA')
+            nome = str(input("Digite o nome: "))
+            idade = leia_inteiro('Digite a idade: ')
+            cadastrar(nome_arquivo, nome, idade)
         elif resposta == 3:
             rotulo("Saindo do sistema")
             print("[bold green]Até logo![/bold green]".center(70))
             break
         else:
             os.system('cls')
-            print("[red]Falha! Digite uma opção válida.[/red]")
+            print("[red]Falha! Digite uma opção válida.[/red]".center(55))
         sleep(2)
